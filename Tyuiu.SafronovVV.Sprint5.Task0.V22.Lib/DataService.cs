60f34aea1e6679d943c50cdb481afe47407b7a72
X-Ray2003 <134064048+X-Ray2003@users.noreply.github.com>
2023-11-17 21:27:56 +0500

    Add project files.

---
+++ Tyuiu.SafronovVV.Sprint5.Task0.V22.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.SafronovVV.Sprint5.Task0.V22.Lib
+{
+    public class DataService : ISprint5Task0V22
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+
+            double z = (Math.Pow(1.0 - (double)x, 2.0)) / ((-3.0) * (double)x);
+
+            z = Math.Round(z, 3);
+            File.WriteAllText(path, Convert.ToString(z));
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.SafronovVV.Sprint5.Task0.V22/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using Tyuiu.SafronovVV.Sprint5.Task0.V22.Lib;
+
+namespace Tyuiu.SafronovVV.Sprint5.Task0.V22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int z = 3;
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Сафронов В. В. | АСОиУб-23-1";
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                    *");
+            Console.WriteLine("* Класс File. Запись данных в текстовый файл                                   *");
+            Console.WriteLine("* Задание #0                                                                   *");
+            Console.WriteLine("* Вариант #22                                                                  *");
+            Console.WriteLine("* Выполнил: Сафронов Владислав Валерьевич | АСОиУб-23-1                        *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                     *");
+            Console.WriteLine("* Дано выражение вычислить его значение при x = 3                              *");
+            Console.WriteLine("* Результат сохранить в текстовый файл OutPutFileTask0.txt                     *");
+            Console.WriteLine("*                                                                              *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                             *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("*              (1-x)^2                                                         *");
+            Console.WriteLine("*     y (x) = ----------                                                       *");
+            Console.WriteLine("*               -3x                                                            *");
+            Console.WriteLine("********************************************************************************");
+
+
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                 *");
+            Console.WriteLine("******************************************************************************");
+
+            string res = ds.SaveToFileTextData(z);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан");
+            Console.ReadKey();
+        }
+    }
+}
