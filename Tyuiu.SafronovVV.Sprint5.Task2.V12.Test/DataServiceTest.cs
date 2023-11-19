23c2d03b1f24c1a000f2f308d0bd2f61fde20105
X-Ray2003 <134064048+X-Ray2003@users.noreply.github.com>
2023-11-19 12:54:33 +0500

    Task2

---
+++ Tyuiu.SafronovVV.Sprint5.Task2.V12.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+using Tyuiu.SafronovVV.Sprint5.Task2.V12.Lib;
+
+namespace Tyuiu.SafronovVV.Sprint5.Task2.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\Black_Reaper\source\repos\Tyuiu.SafronovVV.Sprint5\Tyuiu.SafronovVV.Sprint5.Task2.V12\bin\Debug\OutPutFileTask2.csv";
+            FileInfo fi = new FileInfo(path);
+            bool fileExists = fi.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
